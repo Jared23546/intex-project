6384527714247337902f2bfc6ccf78419fed1210
Jared Christensen <90413445+Jared23546@users.noreply.github.com>
2021-12-01 12:14:54 -0700

    changed back app name

--- Intex/settings.py
+++ Intex/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'Opioids.apps.OpioidsConfig',
+    'Opiods.apps.OpiodsConfig',
 ]
 
 MIDDLEWARE = [
--- Opiods/apps.py
+++ Opiods/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class OpioidsConfig(AppConfig):
+class OpiodsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'Opiods'
